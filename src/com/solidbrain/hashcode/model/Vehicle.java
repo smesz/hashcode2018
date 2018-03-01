40c776034a3a2e606ff9569e5c4f0b0d528fc0d7
Adam Mucha <adam.mucha@pl.abb.com>
2018-03-01 19:42:29 +0100

    mucha

--- src/com/solidbrain/hashcode/model/Vehicle.java
+++ src/com/solidbrain/hashcode/model/Vehicle.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.solidbrain.hashcode.model.Ride;
 
 public class Vehicle {
-	private List<Ride> rides;
+	public List<Ride> rides;
 
 	public List<Ride> getRides() {
 		return rides;
